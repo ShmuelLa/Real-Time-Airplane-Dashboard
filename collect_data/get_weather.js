05091f74a412220814ebaf864692d09f665ce4d4
mor234 <moriya264@gmail.com>
2022-09-04 16:01:19 +0300

    add promise to get from database

--- collect_data/get_weather.js
+++ collect_data/get_weather.js
@@ -6,6 +6,9 @@ const sqlite3 = require('sqlite3').verbose();
 
 
 function get_lat_lon_for_airport(iata_code,icao_code){
+    return new Promise((resolve, reject) => {
+
+	
     var airports_type_string=null;
     var airports_code_string=null
     try {
@@ -18,22 +21,26 @@ function get_lat_lon_for_airport(iata_code,icao_code){
             airports_type_string="icao_code";
             airports_code_string=icao_code;
         }
-        else {return (null,null);}
+        else {reject(new Error ("Hasn't recived any airport id."));}
 
         const db = new sqlite3.Database('./global_airports_sqlite.db');  
         console.log("SELECT lat_decimal, lon_decimal FROM 'airports'  where "+airports_type_string+" ='"+airports_code_string+"';");                                                                                                                    
-        db.each(`SELECT lat_decimal, lon_decimal FROM 'airports'  where "${airports_type_string}"='${airports_code_string}';`, function(err, row) {
-            if(err) return console.log(err.message);
+        db.get(`SELECT lat_decimal, lon_decimal FROM 'airports'  where "${airports_type_string}"='${airports_code_string}';`, function (err, row) {
+            if (err) reject(err); 
             console.log(row.lat_decimal + ": " + row.lon_decimal);
+            resolve (row);
         });
 
 
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        return null,null;
     }
-   
+}); 
 }
-// get_lat_lon_for_airport("TLV");
+
+get_lat_lon_for_airport("TLV").then((row)=>{console.log(row.lat_decimal, row.lon_decimal);});
+
 // get_lat_lon_for_airport(null,"LLBG");
 // get_lat_lon_for_airport("TLV","LLBG");
 // get_lat_lon_for_airport();
